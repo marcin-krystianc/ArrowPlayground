44f67aa4e7c055509d13aac89b415a35c6f01599
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-09-12 12:41:30 +0200

    benchmark

--- ColumnReadingPerf/ColumnReadingPerf.cc
+++ ColumnReadingPerf/ColumnReadingPerf.cc
@@ -1,20 +1,3 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
-
 #include "arrow/api.h"
 #include "arrow/io/api.h"
 #include "arrow/result.h"
@@ -24,70 +7,111 @@
 
 #include <iostream>
 #include <list>
+#include <chrono>
 
 using arrow::Status;
 
 namespace
 {
+  const char *FILE_NAME = "my.parquet";
 
-  Status WriteTableToParquet(const std::shared_ptr<arrow::Table> &table, const std::string &filename)
+  Status WriteTableToParquet(const std::shared_ptr<arrow::Table> &table, const std::string &filename, std::chrono::microseconds *dt)
   {
-    /*
-    // Choose compression
-    std::shared_ptr<parquet::WriterProperties> props = parquet::WriterProperties::Builder().compression(arrow::Compression::SNAPPY)->build();
-
-    // Opt to store Arrow schema for easier reads back into Arrow
-    std::shared_ptr<parquet::ArrowWriterProperties> arrow_props = parquet::ArrowWriterProperties::Builder().store_schema()->build();
-
-    std::shared_ptr<arrow::io::FileOutputStream> outfile;
-    PARQUET_THROW_NOT_OK(arrow::io::FileOutputStream::Open(filename, &outfile));
-
-    ARROW_RETURN_NOT_OK(parquet::arrow::WriteTable(*table,
-                                                   arrow::default_memory_pool(), outfile,
-                                                   3, props, arrow_props));
+    auto begin = std::chrono::steady_clock::now();
+    auto result = arrow::io::FileOutputStream::Open(filename);
+    auto outfile = result.ValueOrDie();
+    PARQUET_THROW_NOT_OK(parquet::arrow::WriteTable(*table, arrow::default_memory_pool(), outfile, 100000));
+    auto end = std::chrono::steady_clock::now();
+    *dt = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
     return Status::OK();
-*/
+  }
 
-        std::shared_ptr<arrow::io::FileOutputStream> outfile;
-        PARQUET_THROW_NOT_OK(arrow::io::FileOutputStream::Open(filename, &outfile));
-        PARQUET_THROW_NOT_OK(parquet::arrow::WriteTable(*table.get(), arrow::default_memory_pool(), outfile, 1024));
-        return Status::OK();
+  std::shared_ptr<arrow::Table> GetTable(size_t n)
+  {
+    auto nRows = 10000;
+    std::vector<std::shared_ptr<arrow::Array>> arrays;
+    std::vector<std::shared_ptr<arrow::Field>> fields;
+    arrow::Int32Builder builder = arrow::Int32Builder();
 
+    // For simplicity, we'll create int32 columns. You can expand this to handle other types.
+    for (int i = 0; i < n; i++)
+    {
+      arrow::Int32Builder builder;
+      for (auto j = 0; j < nRows; j++)
+      {
+        if (!builder.Append(j).ok())
+          throw std::runtime_error("builder.Append");
+      }
+
+      std::shared_ptr<arrow::Array> array;
+      if (!builder.Finish(&array).ok())
+        throw std::runtime_error("builder.Finish");
+
+      arrays.push_back(array);
+      fields.push_back(arrow::field("int_column_" + std::to_string(i), arrow::int32()));
+    }
+
+    auto table = arrow::Table::Make(arrow::schema(fields), arrays);
+    return table;
+  }
+
+  Status ReadEntireTable(const std::string &filename, std::chrono::microseconds *dt)
+  {
+    auto begin = std::chrono::steady_clock::now();
+
+    std::shared_ptr<arrow::io::ReadableFile> infile;
+    ARROW_ASSIGN_OR_RAISE(infile, arrow::io::ReadableFile::Open(filename));
+    std::unique_ptr<parquet::arrow::FileReader> reader;
+    ARROW_RETURN_NOT_OK(parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &reader));
+    std::shared_ptr<arrow::Table> parquet_table;
+    // Read the table.
+    PARQUET_THROW_NOT_OK(reader->ReadTable(&parquet_table));
+
+    auto end = std::chrono::steady_clock::now();
+    *dt = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+    return Status::OK();
+  }
+
+  Status ReadColumnsAsTable(const std::string &filename, std::vector<int> indicies, std::chrono::microseconds *dt)
+  {
+    auto begin = std::chrono::steady_clock::now();
+
+    std::shared_ptr<arrow::io::ReadableFile> infile;
+    ARROW_ASSIGN_OR_RAISE(infile, arrow::io::ReadableFile::Open(filename));
+    std::unique_ptr<parquet::arrow::FileReader> reader;
+    ARROW_RETURN_NOT_OK(parquet::arrow::OpenFile(infile, arrow::default_memory_pool(), &reader));
+    std::shared_ptr<arrow::Table> parquet_table;
+    // Read the table.
+    PARQUET_THROW_NOT_OK(reader->ReadTable(indicies, &parquet_table));
+
+    auto end = std::chrono::steady_clock::now();
+    *dt = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
+    return Status::OK();
   }
 
   Status RunMain(int argc, char **argv)
   {
-    auto nRows = 1;
-    std::list<int> nColumns = {100, 200, 300, 500, 1000, 10000};
+    std::list<int> nColumns = {100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000};
+    std::vector<int> indicies(100);
+    std::iota(indicies.begin(), indicies.end(), 0);
 
     for (int n : nColumns)
     {
-      std::cerr << "* n1:" << n << std::endl;
-      std::vector<std::shared_ptr<arrow::Array>> arrays;
-      std::vector<std::shared_ptr<arrow::Field>> fields;
-      arrow::Int32Builder builder = arrow::Int32Builder();
+      auto table = GetTable(n);
+      std::chrono::microseconds writing_dt;
+      ARROW_RETURN_NOT_OK(WriteTableToParquet(table, FILE_NAME, &writing_dt));
 
-      // For simplicity, we'll create int32 columns. You can expand this to handle other types.
-      for (int i = 0; i < n; i++)
-      {
-        arrow::Int32Builder builder;
-        for (auto j = 0; j < nRows; j++)
-        {
-          ARROW_RETURN_NOT_OK(builder.Append(j));
-        }
+      std::chrono::microseconds reading_all_dt;
+      ARROW_RETURN_NOT_OK(ReadEntireTable(FILE_NAME, &reading_all_dt));
 
-        std::shared_ptr<arrow::Array> array;
-        ARROW_RETURN_NOT_OK(builder.Finish(&array));
+      std::chrono::microseconds reading_100_dt;
+      ARROW_RETURN_NOT_OK(ReadColumnsAsTable(FILE_NAME, indicies, &reading_100_dt));
 
-        arrays.push_back(array);
-        fields.push_back(arrow::field("int_column_" + std::to_string(i), arrow::int32()));
-      }
-
-      auto table = arrow::Table::Make(arrow::schema(fields), arrays);
-
-      ARROW_RETURN_NOT_OK(WriteTableToParquet(table, "my.parquet"));
-
-      std::cerr << "* n4:" << n << std::endl;
+      std::cerr << "n=" << n
+                << " ,writing_dt=" << writing_dt.count() / n
+                << " ,reading_all_dt=" << reading_all_dt.count() / n
+                << " ,reading_100_dt=" << reading_100_dt.count() / 100
+                << std::endl;
     }
 
     return Status::OK();
