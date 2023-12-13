a26b192c928903eda173e80767a57f947038793d
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-12-13 10:55:19 +0100

    rapidparquet

--- RapidParquet/python/setup.py
+++ RapidParquet/python/setup.py
@@ -10,27 +10,21 @@ from Cython.Build import cythonize
 import pyarrow
 import numpy
 
-CURRENT_PYTHON = sys.version_info[:2]
-REQUIRED_PYTHON = (3, 7)
-
-if CURRENT_PYTHON < REQUIRED_PYTHON:
-    sys.stderr.write(
-        """
-==========================
-Unsupported Python version
-==========================
-This version of RapidParquet requires at least Python {}.{}, but
-you're trying to install it on Python {}.{}. To resolve this,
-consider upgrading to a supported Python version.
-
-If you can't upgrade your Python version, you'll need to
-pin to an older version of Requests (<2.28).
-""".format(
-            *(REQUIRED_PYTHON + CURRENT_PYTHON)
-        )
-    )
-    sys.exit(1)
-
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
 
 class PyTest(TestCommand):
     user_options = [("pytest-args=", "a", "Arguments to pass into py.test")]
@@ -55,8 +49,6 @@ class PyTest(TestCommand):
         errno = pytest.main(self.pytest_args)
         sys.exit(errno)
 
-
-
 about = {}
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, "rapidparquet", "__version__.py"), "r", "utf-8") as f:
@@ -67,13 +59,21 @@ with open("README.md", "r", "utf-8") as f:
 
 # Define your extension
 extensions = [
-    Extension(
-        "rapidparquet.my_cython_code",
-        ["rapidparquet/my_cython_code.pyx"],
+    Extension( "rapidparquet", ["rapidparquet/rapid_parquet_cython.pyx"],
         include_dirs = [pyarrow.get_include(), numpy.get_include()],  
         library_dirs = pyarrow.get_library_dirs(),
+        language = "c++",
     )
 ]
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0))) and cythonize is not None
+CYTHONIZE = 1
+
+if CYTHONIZE:
+    compiler_directives = {"language_level": 3, "embedsignature": True}
+    extensions = cythonize(extensions, compiler_directives=compiler_directives)
+else:
+    extensions = no_cythonize(extensions)
 
 # Make default named pyarrow shared libs available.
 pyarrow.create_library_symlinks()
@@ -88,16 +88,12 @@ setup(
     author_email=about["__author_email__"],
     url=about["__url__"],
     packages=["rapidparquet"],
-    package_data={"": ["LICENSE", "NOTICE"]},
     package_dir={"": "."},
     include_package_data=True,
     python_requires=">=3.7",
-    setup_requires="pyarrow>=10,",
-    install_requires="pyarrow>=10",
-    extras_require={"arrow": ["pyarrow>=7.0,<15"], "numpy": "numpy>=1.20.0"},
-    license=about["__license__"],
+    setup_requires=["pyarrow>=10","cython>=3"],
     zip_safe=False,
-    ext_modules=cythonize(extensions),    
+    ext_modules=extensions,    
     project_urls={
         "Documentation": "https://github.com/marcin-krystianc/ArrowPlayground",
         "Source": "https://github.com/marcin-krystianc/ArrowPlayground"
